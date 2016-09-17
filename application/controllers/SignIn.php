ce6c8d2ff711cdc9d92455f257239c562967cb67
dwikyputra <dwiky.01.oke@gmail.com>
2016-09-17 20:49:40 +0700

    Update SignIn.php

--- application/controllers/SignIn.php
+++ application/controllers/SignIn.php
@@ -21,7 +21,6 @@ class SignIn extends CI_Controller {
 
 	public function index()
 	{ 
-		$this->load->library('session');
 		$this->load->view('header');
 		$this->load->view('signin_page');
 		$this->load->view('footer');
